be40c002036ecff01f555c866821683369e8650b
Anonymous <>
2021-06-26 20:40:09 -0700

    more changes

---
+++ network_adapter_tweaks.ps1
@@ -0,0 +1,36 @@
+Disable-NetAdapterIPsecOffload -Name '*'
+Disable-NetAdapterPowerManagement -Name '*'
+Disable-NetAdapterQos -Name '*'
+Disable-NetAdapterChecksumOffload -Name '*'
+Disable-NetAdapterLso -Name '*'
+Disable-NetAdapterRsc -Name '*'
+Set-NetAdapterAdvancedProperty -Name '*' -DisplayName 'Adaptive Inter-Frame Spacing' -RegistryValue '0'
+Set-NetAdapterAdvancedProperty -Name '*' -DisplayName 'ARP Offload' -RegistryValue '0'
+Set-NetAdapterAdvancedProperty -Name '*' -DisplayName 'Enable PME' -RegistryValue '0'
+Set-NetAdapterAdvancedProperty -Name '*' -DisplayName 'Energy Efficient Ethernet' -RegistryValue '0'
+Set-NetAdapterAdvancedProperty -Name '*' -DisplayName 'Flow Control' -RegistryValue '0'
+Set-NetAdapterAdvancedProperty -Name '*' -DisplayName 'Interrupt Moderation Rate' -RegistryValue '0'
+Set-NetAdapterAdvancedProperty -Name '*' -DisplayName 'Interrupt Moderation' -RegistryValue '0'
+Set-NetAdapterAdvancedProperty -Name '*' -DisplayName 'IPv4 Checksum Offload' -RegistryValue '0'
+Set-NetAdapterAdvancedProperty -Name '*' -DisplayName 'Jumbo Packet' -RegistryValue '0'
+Set-NetAdapterAdvancedProperty -Name '*' -DisplayName 'Large Send Offload V2 (IPv4)' -RegistryValue '0'
+Set-NetAdapterAdvancedProperty -Name '*' -DisplayName 'Large Send Offload V2 (IPv6)' -RegistryValue '0'
+Set-NetAdapterAdvancedProperty -Name '*' -DisplayName 'Packet Priority ^& VLAN' -RegistryValue '0'
+Set-NetAdapterAdvancedProperty -Name '*' -DisplayName 'Protocol NS Offload' -RegistryValue '0'
+Set-NetAdapterAdvancedProperty -Name '*' -DisplayName 'Receive Buffers' -RegistryValue '2048'
+Set-NetAdapterAdvancedProperty -Name '*' -DisplayName 'Receive Side Scaling' -RegistryValue '0'
+Set-NetAdapterAdvancedProperty -Name '*' -DisplayName 'Reduce Speed On Power Down' -RegistryValue '0'
+Set-NetAdapterAdvancedProperty -Name '*' -DisplayName 'System Idle Power Saver' -RegistryValue '0'
+Set-NetAdapterAdvancedProperty -Name '*' -DisplayName 'TCP Checksum Offload (IPv4)' -RegistryValue '0'
+Set-NetAdapterAdvancedProperty -Name '*' -DisplayName 'TCP Checksum Offload (IPv6)' -RegistryValue '0'
+Set-NetAdapterAdvancedProperty -Name '*' -DisplayName 'Transmit Buffers' -RegistryValue '2048'
+Set-NetAdapterAdvancedProperty -Name '*' -DisplayName 'UDP Checksum Offload (IPv4)' -RegistryValue '0'
+Set-NetAdapterAdvancedProperty -Name '*' -DisplayName 'UDP Checksum Offload (IPv6)' -RegistryValue '0'
+Set-NetAdapterAdvancedProperty -Name '*' -DisplayName 'Ultra Low Power Mode' -RegistryValue '0'
+Set-NetAdapterAdvancedProperty -Name '*' -DisplayName 'Speed "&" Duplex' -RegistryValue '6'
+Set-NetTCPSetting -SettingName internet -EcnCapability enabled
+Set-NetOffloadGlobalSetting -ReceiveSideScaling disabled
+Set-NetOffloadGlobalSetting -ReceiveSegmentCoalescing disabled
+Set-NetOffloadGlobalSetting -Chimney disabled
+Set-NetOffloadGlobalSetting -TaskOffload disabled
+Set-NetOffloadGlobalSetting -PacketCoalescingFilter disabled
